3bfbd66d18dfe9e9e54116e2fdc6e4192e6b246b
001-LUIZ.TRINDADE-Informatica <luiz.trindade@cooperalfa.coop.br>
2025-04-02 15:25:44 -0300

    Ajustes iniciais

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+// Styles
+import "./App.css";
+
+// Context
+import { AuthProvider } from "./context/AuthProvider";
+
+// Routes
+import { Rotas } from "./routes/Rotas.jsx";
+
+function App() {
+  return (
+    <>
+      <Rotas />
+    </>
+  );
+}
+
+export default App;
